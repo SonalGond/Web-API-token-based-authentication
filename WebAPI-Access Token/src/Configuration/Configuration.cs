c676df890557cd8a0081b66b49decb61cb1bac71
Sonal Gond <gond.sonal@gmail.com>
2022-01-30 01:49:26 +0530

    Created class

---
+++ WebAPI-Access Token/src/Configuration/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_Access_Token.src.Configuration
+{
+    public class Configuration
+    {
+        public string DomainUrl { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string ClientSecret { get; set; }
+        public string OauthClientKey { get; set; }
+        public string JwtTtlInSeconds { get; set; }
+        public string TimeOutInSeconds { get; set; }
+        public string UrlDigestSalt { get; set; }
+    }
+}
---
+++ WebAPI-Access Token/src/Configuration/ConfigurationProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebAPI_Access_Token.src.Configuration
+{
+    public interface IConfigurationProvider
+    {
+        Task<ConfigurationProvider> GetConfiguration();
+    }
+
+    public class ConfigurationProvider : IConfigurationProvider
+    {
+        //private readonly IParameterProvider _parameterProvider;
+        //private readonly ILogger<ConfigurationProvider> _logger;
+
+        public Task<ConfigurationProvider> GetConfiguration()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
